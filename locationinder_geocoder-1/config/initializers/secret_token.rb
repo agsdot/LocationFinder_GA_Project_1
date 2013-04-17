e744a50ca4562bb4668b9d53f3edaf0b790f840a
Jonathan Lai <jonathanlai@eltux.net>
2013-04-16 23:45:26 -0700

    Two directories added: one html css wireframe prototype and one a preliminary rails geocoder app with functionality

---
+++ locationinder_geocoder-1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geocoder1::Application.config.secret_token = '3b8de7182d164aa0f5dffdf7bb3aefa64b36e242a86ea45584d6b687a3702a5ca537c0f5425ef77275179c0559993eb3ecdb365538acfdd7456ec4179bd02f90'
